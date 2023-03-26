d4fe047b5200a8c967231ad569d0e3e237160ae3
KaterynaSokol <k.sokol2019@gmail.com>
2023-03-26 17:13:34 +0300

    T2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const items = ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.classList = "item";
+  ingredientItem.textContent = ingredient;
+  return ingredientItem;
+});
+
+list.append(...items);
